3881506f90ef4f96d64649da06581637e4bcbe83
Husam Ibrahim <huielbashir@gmail.com>
2020-02-27 16:44:34 +0200

    plotly: first pass at pie chart

---
+++ src/components/plotly/PieChart/PieChart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="chartdiv"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onBeforeUnmount, ref } from '@vue/composition-api'
+import Plotly from 'plotly.js-dist'
+
+export default defineComponent({
+  setup() {
+    const chartdiv = ref(null)
+    const data = [
+      {
+        values: [19, 26, 55],
+        labels: ['Residential', 'Non-Residential', 'Utility'],
+        type: 'pie',
+      },
+    ]
+    const layout = {
+      height: 400,
+      width: 500,
+    }
+
+    onMounted(() => {
+      Plotly.newPlot(chartdiv.value, data, layout)
+    })
+
+    onBeforeUnmount(() => {
+      Plotly.purge(chartdiv.value)
+    })
+
+    return { chartdiv }
+  },
+})
+</script>
