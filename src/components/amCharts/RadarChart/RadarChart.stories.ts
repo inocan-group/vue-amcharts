a74b809c85b56b0608ba2d16fd9c9908801e984c
Husam Ibrahim <huielbashir@gmail.com>
2020-02-28 17:59:10 +0200

    amCharts: first pass at radar chart

---
+++ src/components/amCharts/RadarChart/RadarChart.stories.ts
@@ -0,0 +1,8 @@
+import RadarChart from './RadarChart.vue'
+
+export default { title: 'amCharts/RadarChart' }
+
+export const radarChart = () => ({
+  components: { RadarChart },
+  template: '<radar-chart/>',
+})
