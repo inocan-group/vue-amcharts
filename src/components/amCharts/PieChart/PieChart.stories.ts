22365ee60825011c604869a9a3ec530e7ce051e4
Husam Ibrahim <huielbashir@gmail.com>
2020-02-28 17:35:53 +0200

    amCharts: first pass at pie chart

---
+++ src/components/amCharts/PieChart/PieChart.stories.ts
@@ -0,0 +1,8 @@
+import PieChart from './PieChart.vue'
+
+export default { title: 'amCharts/PieChart' }
+
+export const pieChart = () => ({
+  components: { PieChart },
+  template: '<pie-chart/>',
+})
