f295112e14c8de380f3d7c2f8599fddfa4f9670d
Husam Ibrahim <huielbashir@gmail.com>
2020-03-06 16:26:27 +0200

    amCharts: make pie chart reactive

--- src/components/amCharts/PieChart/PieChart.vue
+++ src/components/amCharts/PieChart/PieChart.vue
@@ -5,71 +5,69 @@
 <script lang="ts">
 import * as am4core from '@amcharts/amcharts4/core'
 import * as am4charts from '@amcharts/amcharts4/charts'
-import { defineComponent, onMounted, ref, onBeforeUnmount } from '@vue/composition-api'
+import am4themesAnimated from '@amcharts/amcharts4/themes/animated'
+import { defineComponent, onMounted, ref, Ref, onBeforeUnmount, watch } from '@vue/composition-api'
+
+am4core.useTheme(am4themesAnimated)
 
 export default defineComponent({
-  setup() {
+  props: {
+    chartData: {
+      type: Array as () => Record<string, string | number>[],
+      required: true,
+    },
+    category: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+  setup(props) {
     const chartdiv = ref(null)
-    const data = [
-      {
-        country: 'Lithuania',
-        litres: 501.9,
-      },
-      {
-        country: 'Czech Republic',
-        litres: 301.9,
-      },
-      {
-        country: 'Ireland',
-        litres: 201.1,
-      },
-      {
-        country: 'Germany',
-        litres: 165.8,
-      },
-      {
-        country: 'Australia',
-        litres: 139.9,
-      },
-      {
-        country: 'Austria',
-        litres: 128.3,
-      },
-      {
-        country: 'UK',
-        litres: 99,
-      },
-      {
-        country: 'Belgium',
-        litres: 60,
-      },
-      {
-        country: 'The Netherlands',
-        litres: 50,
-      },
-    ]
-    let chart: am4charts.PieChart | undefined = undefined
+    const data = props.chartData
+    const chart: Ref<am4charts.PieChart | null> = ref(null)
 
     onMounted(() => {
       // Create chart instance
-      chart = am4core.create((chartdiv.value as unknown) as HTMLElement, am4charts.PieChart)
+      chart.value = am4core.create((chartdiv.value as unknown) as HTMLElement, am4charts.PieChart)
 
       // Add data
-      chart.data = data
+      chart.value.data = data
 
       // Add and configure Series
-      const pieSeries = chart.series.push(new am4charts.PieSeries())
-      pieSeries.dataFields.value = 'litres'
-      pieSeries.dataFields.category = 'country'
+      const pieSeries = chart.value.series.push(new am4charts.PieSeries())
+      pieSeries.dataFields.category = props.category
+      pieSeries.dataFields.value = props.value
+
+      // Refresh chart
+      watch([() => props.chartData, () => props.category, () => props.value], (newParams, oldParams) => {
+        const [newData, newCategory, newValue] = newParams as [Record<string, string | number>[], string, string]
+
+        if (chart.value) {
+          chart.value.data.forEach((d, i) => {
+            if (d[newCategory] != newData[i][newCategory]) d[newCategory] = newData[i][newCategory]
+            if (d[newValue] != newData[i][newValue]) d[newValue] = newData[i][newValue]
+          })
+
+          if (oldParams) {
+            const [oldData, oldCategory, oldValue] = oldParams as [Record<string, string | number>[], string, string]
+
+            if (newCategory !== oldCategory || newValue !== oldValue) {
+              pieSeries.dataFields.category = newCategory as string
+              pieSeries.dataFields.value = newValue as string
+              chart.value.invalidateData()
+            }
+          }
+
+          chart.value.invalidateRawData()
+        }
+      })
     })
 
     onBeforeUnmount(() => {
-      if (chart) {
-        chart.dispose()
+      if (chart.value) {
+        chart.value.dispose()
       }
     })
 
-    return { chartdiv }
+    return { chartdiv, chart }
   },
 })
 </script>
