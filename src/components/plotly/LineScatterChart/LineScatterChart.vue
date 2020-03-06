a1449336c39494a69956100e33d09cc8114fcd0a
Husam Ibrahim <huielbashir@gmail.com>
2020-03-06 16:36:25 +0200

    plotly: make bar line and scatter charts reactive

--- src/components/plotly/LineScatterChart/LineScatterChart.vue
+++ src/components/plotly/LineScatterChart/LineScatterChart.vue
@@ -3,65 +3,43 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, onBeforeUnmount, ref } from '@vue/composition-api'
-import Plotly from 'plotly.js-dist'
-import { Root, PlotData } from 'plotly.js'
+import { defineComponent, onMounted, onBeforeUnmount, ref, watch } from '@vue/composition-api'
+import Plotly, { Root, PlotData, Layout } from 'plotly.js-dist'
 
 export default defineComponent({
-  setup() {
+  props: {
+    chartData: { type: Array, required: true },
+    layout: Array,
+  },
+  setup(props) {
     const chartdiv = ref(null)
+    const data = props.chartData as Partial<PlotData>[]
 
-    const trace1: Partial<PlotData> & { textfont: object } = {
-      x: [1, 2, 3, 4, 5],
-      y: [1, 6, 3, 6, 1],
-      mode: 'text+markers',
-      type: 'scatter',
-      name: 'Team A',
-      text: ['A-1', 'A-2', 'A-3', 'A-4', 'A-5'],
-      textposition: 'top center',
-      textfont: {
-        family: 'Raleway, sans-serif',
-      },
-      marker: { size: 12 },
-    }
-
-    const trace2: Partial<PlotData> & { textfont: object } = {
-      x: [1.5, 2.5, 3.5, 4.5, 5.5],
-      y: [4, 1, 7, 1, 4],
-      mode: 'text+markers',
-      type: 'scatter',
-      name: 'Team B',
-      text: ['B-a', 'B-b', 'B-c', 'B-d', 'B-e'],
-      textfont: {
-        family: 'Times New Roman',
-      },
-      textposition: 'bottom center',
-      marker: { size: 12 },
-    }
-
-    const data = [trace1, trace2]
-
-    const layout = {
-      xaxis: {
-        range: [0.75, 5.25],
-      },
-      yaxis: {
-        range: [0, 8],
-      },
-      legend: {
-        y: 0.5,
-        yref: 'paper',
-        font: {
-          family: 'Arial, sans-serif',
-          size: 20,
-          color: 'grey',
-        },
-      },
-      title: 'Data Labels on the Plot',
-    }
+    const layout = props.layout as Partial<Layout>
 
     onMounted(() => {
       Plotly.newPlot((chartdiv.value as unknown) as Root, data, layout)
+
+      watch(
+        () => props.chartData,
+        newData => {
+          Plotly.animate(
+            (chartdiv.value as unknown) as Root,
+            {
+              data: newData as Partial<PlotData>[],
+            },
+            {
+              transition: {
+                duration: 300,
+                easing: 'cubic-in-out',
+              },
+              frame: {
+                duration: 300,
+              },
+            },
+          )
+        },
+      )
     })
 
     onBeforeUnmount(() => {
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,7 +3,23 @@ declare module '*.vue' {
   export default Vue
 }
 
+declare module 'rough-viz'
 declare module 'plotly.js-dist' {
-  import Plotly from 'plotly.js'
-  export default Plotly
+  import { Root, Frame } from 'plotly.js'
+  export * from 'plotly.js'
+  interface AnimationAttributes {
+    transition: {
+      duration: number
+      easing: string
+    }
+    frame: {
+      duration: number
+    }
+  }
+
+  export function animate(
+    root: Root,
+    frame: Partial<Frame> | Partial<Frame>[],
+    AnimationAttributes: AnimationAttributes,
+  ): Promise<undefined>
 }
