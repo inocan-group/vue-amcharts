268296882c21c68cb33ecf82c061447608120769
Husam Ibrahim <huielbashir@gmail.com>
2020-03-16 17:11:46 +0200

    plotly: refactor trace setup into composable function

--- src/components/plotly/PlotlyChart/PlotlyPieTrace.vue
+++ src/components/plotly/PlotlyChart/PlotlyPieTrace.vue
@@ -1,34 +1,25 @@
 <script lang="ts">
-import { defineComponent, Ref, computed, watch, inject } from '@vue/composition-api'
+import { defineComponent, PropType, computed, Ref } from '@vue/composition-api'
 import { PlotData } from 'plotly.js-dist'
-import { plotDataSymbol } from '@/shared/plotly'
+import { useSetupPlotlyTrace } from '@/composables/plotly'
 
 export default defineComponent({
   props: {
-    traceData: { type: Object as () => Partial<PlotData>, required: true },
+    traceData: { type: Object as PropType<Partial<PlotData>>, required: true },
   },
   /**
    * Do not destructure context or you'll lose reactivity. This is a bug in the composition API plugin.
    * See https://github.com/vuejs/composition-api/issues/264
    */
   setup(props, context) {
-    const plotData = inject(plotDataSymbol) as Ref<(Partial<PlotData> & { id: number })[]>
-    const id = plotData.value.length
-
     const trace = computed(() => ({
-      id,
       values: props.traceData.values,
       labels: props.traceData.labels,
       ...context.attrs,
       type: 'pie',
-    })) as Ref<Partial<PlotData> & { id: number }>
+    })) as Ref<Partial<PlotData>>
 
-    plotData.value.push(trace.value)
-
-    watch(trace, newTrace => {
-      const index = plotData.value.findIndex(t => t.id === id)
-      plotData.value.splice(index, 1, newTrace)
-    })
+    useSetupPlotlyTrace(trace)
   },
   render(h) {
     return h()
