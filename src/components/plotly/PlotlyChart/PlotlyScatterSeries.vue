9aa105022010b8cd59fd2dd597a9f18266887afc
Husam Ibrahim <huielbashir@gmail.com>
2020-03-23 02:03:53 +0200

    plotly: move from dep injection to context.parent

--- src/components/plotly/PlotlyChart/PlotlyScatterSeries.vue
+++ src/components/plotly/PlotlyChart/PlotlyScatterSeries.vue
@@ -1,26 +1,28 @@
 <script lang="ts">
 import { defineComponent, PropType, computed, Ref } from '@vue/composition-api'
-import { PlotData } from 'plotly.js-dist'
 import { useSetupPlotlySeries } from '@/composables/plotly'
+import { IPlotData, IPlotlyChart } from '@/@types/plotly'
 
 export default defineComponent({
   props: {
-    seriesData: { type: Object as PropType<Partial<PlotData>>, required: true },
+    seriesData: { type: Object as PropType<Pick<IPlotData, 'x' | 'y'>>, required: true },
   },
   /**
    * Do not destructure context or you'll lose reactivity. This is a bug in the composition API plugin.
    * See https://github.com/vuejs/composition-api/issues/264
    */
   setup(props, context) {
+    const plotData = (context.parent as Vue & IPlotlyChart).plotData
+
     const series = computed(() => ({
       x: props.seriesData.x,
       y: props.seriesData.y,
       ...context.attrs,
       type: 'scatter',
       mode: context.attrs.mode ?? 'markers',
-    })) as Ref<Partial<PlotData>>
+    })) as Ref<IPlotData>
 
-    useSetupPlotlySeries(series)
+    useSetupPlotlySeries(series, plotData)
   },
   render(h) {
     return h()
--- src/composables/plotly/index.ts
+++ src/composables/plotly/index.ts
@@ -1,18 +1,16 @@
-import { Ref, inject, watch } from '@vue/composition-api'
-import { PlotData } from 'plotly.js-dist'
-import { plotDataSymbol } from '@/shared/plotly'
+import { Ref, watch } from '@vue/composition-api'
+import { IPlotData } from '@/@types/plotly'
 
-export const useSetupPlotlySeries = (series: Ref<Partial<PlotData> & { id?: number }>) => {
-  const plotData = inject(plotDataSymbol) as Ref<(Partial<PlotData> & { id?: number })[]>
-  const id = plotData.value.length
+export const useSetupPlotlySeries = (series: Ref<IPlotData>, plotData: IPlotData[]) => {
+  const id = plotData.length
 
   series.value.id = id
 
-  plotData.value.push(series.value)
+  plotData.push(series.value)
 
   watch(series, newSeries => {
     newSeries.id = id
-    const index = plotData.value.findIndex(t => t.id === id)
-    plotData.value.splice(index, 1, newSeries)
+    const index = plotData.findIndex(t => t.id === id)
+    plotData.splice(index, 1, newSeries)
   })
 }
