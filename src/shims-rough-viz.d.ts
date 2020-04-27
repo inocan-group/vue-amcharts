f83ef83b20c13178cd37ca94b9fa3b1bad5d44ce
Husam Ibrahim <huielbashir@gmail.com>
2020-04-27 02:49:07 +0200

    chore: delete stale files

--- src/shims-rough-viz.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'rough-viz' {
-  export class Bar {}
-  export class BarH {}
-  export class Donut {}
-  export class Line {}
-  export class Pie {}
-  export class Scatter {}
-  export class StackedBar {}
-}
