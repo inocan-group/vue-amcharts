6677b230f7c3556c0c0fcfdc06481a35e45b765f
Husam Ibrahim <huielbashir@gmail.com>
2020-05-07 02:53:07 +0200

    feat: basic storybook theme

---
+++ config/storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+import logo from '../../src/assets/vue-amcharts-logo.png'
+
+export default create({
+  base: 'light',
+  brandTitle: 'vue-amcharts',
+  brandUrl: 'https://github.com/inocan-group/vue-amcharts',
+  brandImage: logo,
+})
